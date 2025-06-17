53802277851c0a6349582c5b487120b833213e74
ben1maru <evgenbabych1658@gmail.com>
2025-06-17 11:03:53 +0300

    newhost

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://tutors-finders-server.onrender.com/api' // URL вашого backend
+    baseURL: 'https://tutorsfindersserver-production.up.railway.app/api' // URL вашого backend
 });
 
 // Перехоплювач (interceptor) для додавання токена до кожного запиту
